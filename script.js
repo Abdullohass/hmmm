1bde5a862751b4a05c8e46db4cfbb96868b1bc51
Abdullohass <abdulloh.s.gk@gmail.com>
2024-10-22 13:56:58 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// function sum(son1, son2, son3, son4) {
+//     let katta = Math.max(son1, son2, son3, son4)
+//     let kichik = Math.min(son1, son2, son3, son4)
+//     console.log(katta+kichik);
+    
+// }
+// sum(1,2,3,4)
+
+// const btn=document.getElementById("color");
+// btn.addEventListener("click",()=>{
+//     const body=document.getElementsByTagName("body")[0]
+//     body.style.backgroundColor="red"
+// })
+
+
+
+
+function qoragaBoya(){
+    document.body.classList.add("dark");
+}
+function oqgaBoya(){
+    document.body.classList.remove("dark");
+}
+
+
+
+
+
+
